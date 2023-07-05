0688a23b36c353e05134b22e938715e6447d9966
heejoo <heejoo_byun@naver.com>
2023-07-06 00:51:50 +0900

    [Feat] - Timer 드디어 !!!! 디자인에 맞게 구현 연습 완료

--- YELLO_practice/Timer/TimerView.swift
+++ YELLO_practice/Timer/TimerView.swift
@@ -28,14 +28,14 @@ final class TimerView: UIView {
             self.timeLabel.text = String(format: "%02d : %02d", Int(remainingSeconds/60), Int(remainingSeconds.truncatingRemainder(dividingBy: 60)))
         }
     }
-        
+    
     private var circularPath: UIBezierPath {
         UIBezierPath(
             arcCenter: CGPoint(x: self.frame.size.width / 2.0, y: self.frame.size.height / 2.0),
             radius: 100,
-            startAngle: CGFloat(-Double.pi / 2),
-            endAngle: CGFloat(3 * Double.pi / 2),
-            clockwise: true
+            startAngle: CGFloat(3 * Double.pi / 2),
+            endAngle: CGFloat(-Double.pi / 2),
+            clockwise: false // 시계 방향과 반대 방향을 나타내기 위해 clockwise 값을 false로 변경
         )
     }
     
@@ -43,7 +43,7 @@ final class TimerView: UIView {
         super.init(frame: frame)
         setUI()
         
-        start(duration: 120)
+        start(duration: 2400)
     }
     
     required init?(coder: NSCoder) {
@@ -81,7 +81,7 @@ final class TimerView: UIView {
             $0.textColor = .systemGray4
             $0.text = "시간이 지나면 다시 투표할 수 있어요!"
             $0.font = UIFont.boldSystemFont(ofSize: 17)
-
+            
         }
         
         timeLabel.do {
@@ -95,8 +95,9 @@ final class TimerView: UIView {
             $0.fillColor = UIColor.clear.cgColor
             $0.lineCap = .round
             $0.lineWidth = 15.0
-            $0.strokeEnd = 1
-            $0.strokeColor = UIColor.yellow.cgColor
+            $0.strokeStart = 0
+            $0.strokeColor = UIColor.systemGray.cgColor
+            $0.zPosition = 0
         }
         
         progressLayer.do {
@@ -104,20 +105,21 @@ final class TimerView: UIView {
             $0.fillColor = UIColor.clear.cgColor
             $0.lineCap = .round
             $0.lineWidth = 15.0
-            $0.strokeEnd = 0
-            $0.strokeColor = UIColor.systemGray.cgColor
+            $0.strokeStart = 0
+            $0.strokeEnd = 1
+            $0.strokeColor = UIColor.yellow.cgColor
+            $0.zPosition = 1
         }
-        
     }
     
     func setLayout() {
         self.addSubviews(self.infoLabel, self.infoText, self.timeLabel)
         self.layer.addSublayer(self.backgroundLayer)
         self.layer.addSublayer(self.progressLayer)
-
+        
         infoLabel.snp.makeConstraints {
             $0.centerX.equalToSuperview()
-            $0.top.equalToSuperview().inset(180)
+            $0.top.equalToSuperview().inset(150)
         }
         
         infoText.snp.makeConstraints {
@@ -131,40 +133,43 @@ final class TimerView: UIView {
         
     }
     
-   
+    
     func start(duration: TimeInterval) {
-      self.remainingSeconds = duration
-      
-      // timer
-      self.timer?.invalidate()
-      let startDate = Date()
-      self.timer = Timer.scheduledTimer(
-        withTimeInterval: 1,
-        repeats: true,
-        block: { [weak self] _ in
-          let remainingSeconds = duration - round(abs(startDate.timeIntervalSinceNow))
-          guard remainingSeconds >= 0 else {
-            self?.stop()
-            return
-          }
-          self?.remainingSeconds = remainingSeconds
-        }
-      )
-      
-      // animation
-      self.progressLayer.removeAnimation(forKey: self.animationName)
-      let circularProgressAnimation = CABasicAnimation(keyPath: "strokeEnd")
-      circularProgressAnimation.duration = duration
-      circularProgressAnimation.toValue = 1
-      circularProgressAnimation.fillMode = .forwards
-      circularProgressAnimation.isRemovedOnCompletion = false
-      self.progressLayer.add(circularProgressAnimation, forKey: self.animationName)
+        self.remainingSeconds = duration
+        // timer
+        self.timer?.invalidate()
+        let startDate = Date()
+        self.timer = Timer.scheduledTimer(
+            withTimeInterval: 1,
+            repeats: true,
+            block: { [weak self] _ in
+                let elapsedSeconds = round(abs(startDate.timeIntervalSinceNow))
+                let remainingSeconds = max(duration - elapsedSeconds, 0)
+                guard remainingSeconds > 0 else {
+                    self?.stop()
+                    return
+                }
+                self?.remainingSeconds = remainingSeconds
+                self?.animateProgress(to: Float(remainingSeconds / duration))
+            }
+        )
     }
-        
+    
     func stop() {
-      self.timer?.invalidate()
-      self.progressLayer.removeAnimation(forKey: self.animationName)
-      self.remainingSeconds = 120
+        self.timer?.invalidate()
+        self.progressLayer.removeAnimation(forKey: self.animationName)
+        self.remainingSeconds = 2400
+    }
+    
+    func animateProgress(to value: Float) {
+        self.progressLayer.removeAnimation(forKey: self.animationName)
+        let circularProgressAnimation = CABasicAnimation(keyPath: "strokeEnd")
+        circularProgressAnimation.duration = 1
+        circularProgressAnimation.fromValue = self.progressLayer.presentation()?.strokeEnd ?? 1.0
+        circularProgressAnimation.toValue = value
+        circularProgressAnimation.fillMode = .forwards
+        circularProgressAnimation.isRemovedOnCompletion = false
+        self.progressLayer.add(circularProgressAnimation, forKey: self.animationName)
     }
     
 }
